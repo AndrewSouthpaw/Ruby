5aec4d9a8b534ae1dc041566c41655746d54fa0e
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-11-20 19:23:25 +0100

    objects and classes

--- Sandbox-Ruby.rb
+++ Sandbox-Ruby.rb
@@ -1,11 +1,26 @@
 
-class Hello
+class A
+  def m1
+    34
+  end
 
-  def my_first_method
-    puts "Hello, World!"
+  def m2 (x,y)
+    z = 7
+    if x > y
+      false
+    else
+      x + y * z
+    end
   end
 
 end
+    
+class B
+  def m1
+    4
+  end
 
-x = Hello.new
-x.my_first_method
+  def m3 x
+    x.abs * 2 + self.m1
+  end
+end
