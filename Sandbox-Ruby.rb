05b33a65953785f836d29f0f0860427759d1f7e9
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-11-22 13:10:04 +0100

    hash and ranges

--- Sandbox-Ruby.rb
+++ Sandbox-Ruby.rb
@@ -1,11 +1,16 @@
-	a = [3,5,7,9]
-	i = b.count {|x| x >= 6 }
-	# don't need first-class function for this
-	c = a.map {|x| {|y| x >= y } } #want array where ith is function that takes
-        #arg y, returns true if a in that position is >=
-        #to that value
-	# blocks can't do this!
-	c = a.map {|x| (lambda {|y| x >= y}) }
-	c[2].call #error, expects 1 argument
-	c[2].call 17  => false
-	c.count {|x| x.call(5) } => 3
+# empty hash {}
+h1 = {}
+h1["a"] = "Found A"
+h1[false] = "Found false"
+h1 => {"a"=>"Found A", false=>"Found false"}
+h1[42] => nil
+h1.keys => {"a", false}
+h1.values => {"Found A", "Found false"}
+h2 = {"SML"=>1, "Racket"=>2, "Ruby"=>3}
+h2.size => 3
+h2.each {|k,v| print k; print ": "; puts v}
+# symbols:   :foo
+
+# ranges
+1..1000000 => 1..1000000  # super fast!  It's an object.
+(1..100).inject {|acc,elt| acc + elt}	=> 5050
