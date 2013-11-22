50fd7ad4b68d303b74e074298d359556d3b0fff1
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-11-22 13:44:30 +0100

    overriding, dynamic dispatch

--- Sandbox-Ruby.rb
+++ Sandbox-Ruby.rb
@@ -15,12 +15,47 @@ class Point
 
 end
 
-class ColorPoint < Point
-  attr_accessor :color
+class ThreeDPoint < Point
+  attr_accessor :z
 
-  def initialize(x,y,c="clear")
-    super(x,y) #keyword super calls same method in superclass
-    @color = c
+  def initialize(x,y,z)
+    super(x,y)
+    @z = z
+  end
+  def distFromOrigin
+    d = super
+    Math.sqrt(d * d + @z * @z)
+  end
+  def distFromOrigin2
+    d = super
+    Math.sqrt(d * d + z * z)
   end
 end
+
+class PolarPoint < Point
+  # by not calling super cons, no x and y ivars
+  # In Java/C# would just have unused x, y fields
+  def initialize(r,theta)
+    @r = r
+    @theta = theta
+  end
+  def x
+    @r * Math.cos(@theta)
+  end
+  def y
+    @r * Math.sin(@theta)
+  end
+  def x= a
+    b = y
+    @theta = ...
+    @r = ()
+    self
+  end
+  def y= b
+  end
+  def distFromOrigin
+    @r
+  end
+  # inherited distFromOrigin2 works as is!
+  # because self.x and self.y (called in super) point to methods in subclass. Whoa.
 
