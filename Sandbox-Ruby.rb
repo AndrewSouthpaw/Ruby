4b6e152ab04c2bdd0f616990da3ccb09594cbb7d
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-12-10 10:47:46 +0000

    sandbox

--- Sandbox-Ruby.rb
+++ Sandbox-Ruby.rb
@@ -1,27 +1,72 @@
+# class A
+#   attr_accessor :x
+#   def m1
+#     @x = 4
+#   end
+#   def m2
+#     m1
+#     @x > 4
+#   end
+#   def m3
+#     @x = 4
+#     @x > 4
+#   end
+#   def m4
+#     self.x = 4
+#     @x > 4
+#   end
+# end
+
+# class B < A
+#   def x= (n)
+#     @x = 5
+#   end
+#   @x = 5
+
+#   def m1
+#     @x = 5
+#   end
+# end
+
+
+class MyRange
+  include Enumerable
+  def initialize(low,high)
+    @low = low
+    @high = high
+  end
+  def each
+    i=@low
+    while i <= @high
+      puts "hello"
+      yield i
+      i=i+1
+    end
+  end
+end
+
+
 class A
-  def m1
-    self.m2()
+  def initialize a
+    @arr = a
   end
-  def m2
-    puts "Hello"
+  def get i
+    @arr[i]
   end
-end
-module M
-  def m3
-    self.m4()
+  def sum
+    @arr.inject(0) {|acc,x| acc + x}
   end
 end
+
 class B < A
-  def m2
-    puts "Hello"
+  def initialize a
+    super
+    @ans = false
+  end
+  def sum
+    if !@ans
+      @ans = @arr.inject(0) {|acc,x| acc + x}
+    end
+    @ans
   end
 end
-class C < A
-  include M
-  def m4
-    puts "Hello"
-  end
-end
-class D < B
-  include M
-end 
